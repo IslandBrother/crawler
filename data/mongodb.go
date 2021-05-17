15d68a3dca80c0640bbba80997404a0478517ba1
john <lord1229@gmail.com>
2021-05-17 23:53:33 +0900

    mongo db 연결 예제 코드 추가

---
+++ data/mongodb.go
@@ -0,0 +1,11 @@
+package data
+
+import (
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+    "go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+defer cancel()
+client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/island-brother/crawler
 
 go 1.16
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/stretchr/testify v1.7.0
+	go.mongodb.org/mongo-driver v1.5.2
+)
