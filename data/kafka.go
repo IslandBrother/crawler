0dc4b3f8d95b6701142f7c14cb53c31201d8cd9d
john <lord1229@gmail.com>
2021-05-22 23:48:55 +0900

    kafka producer 다른 모듈에서 가져다 쓸 수 있도록 수정.

--- data/kafka.go
+++ data/kafka.go
@@ -4,11 +4,10 @@ import (
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 )
 
-func produce(topic string, data string) {
+func KafkaProducer() *kafka.Producer {
 	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
 	if err != nil {
 		panic(err)
 	}
-
-	defer p.Close()
+	return p
 }
