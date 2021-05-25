65a6401aba08dc3e338329cac578d45a108891ab
john <lord1229@gmail.com>
2021-05-25 23:58:26 +0900

    mongodb build 시에 에러나는 부분 임시 주석 처리

--- data/mongodb.go
+++ data/mongodb.go
@@ -1,11 +1,16 @@
 package data
 
-import (
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-    "go.mongodb.org/mongo-driver/mongo/readpref"
-)
+// import (
+// 	"context"
 
-ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-defer cancel()
-client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+// 	"go.mongodb.org/mongo-driver/mongo"
+// 	"go.mongodb.org/mongo-driver/mongo/options"
+// 	"go.mongodb.org/mongo-driver/mongo/readpref"
+// )
+
+func mogodb() {
+	// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	// defer cancel()
+	// client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	// return
+}
