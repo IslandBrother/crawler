0e45a8e161b1d2e9e1ed11b9a15c490c858bbfab
ironprayer <ironprayer@naver.com>
2021-06-04 22:41:21 +0900

    6월 첫 째주 작업 Parser 업데이트

--- parser/parser.go
+++ parser/parser.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
-	. "github.com/island-brother/crawler/common"
 	"github.com/PuerkitoBio/goquery"
+	. "github.com/island-brother/crawler/common"
 )
 
 func GetContent(html string) {
@@ -14,15 +14,16 @@ func GetContent(html string) {
 
 	if err != nil {
 		log.Fatal(err)
-	})
+	}
 
-	title := getTitleInDocument(doc)
-	description := getDescriptionInDocument(doc)
+	//title := getTitleInDocument(doc)
+	//description := getDescriptionInDocument(doc)
 	imgInfos := getImgDatasInDocument(doc)
-	links := getURLsInDocument(doc)
-	
-	fmt.Println(title, description, imgInfos, links)
-	
+	links := getURLsInDocument(doc, pageURL)
+	writeLinkInFile(links)
+	writeImgInFile(imgInfos)
+	//fmt.Println(title, description, links)
+
 }
 
 func getTitleInDocument(doc *goquery.Document) string {
@@ -43,41 +44,58 @@ func getDescriptionInDocument(doc *goquery.Document) string {
 	return description
 }
 
-func getImgDatasInDocument(doc *goquery.Document) []imgInfo {
-	var imgInfos []imgInfo
+// img 태그와 picture 태그를 각자 다룰지 아니면 한 번에 다룰지 각 태그의 속성을 확인을 해봐야겠다.
+func getImgDatasInDocument(doc *goquery.Document) []ImgInfo {
+	var imgInfos []ImgInfo
 	docImgs := doc.Find("img")
-	docPicture := doc.Find("picture")
 
-	// 함수로 만들어서 1개로 줄일 거 생각해야 함.
 	docImgs.Each(func(i int, img *goquery.Selection) {
 		urlOfImg, _ := img.Attr("src")
 		altOfImg, _ := img.Attr("alt")
 		imgInfos = append(imgInfos, ImgInfo{urlOfImg, altOfImg})
 	})
 
-	docPicture.Each(func(i int, pic *goquery.Selection) {
-		urlOfImg, _ := pic.Attr("src")
-		altOfImg, _ := pic.Attr("alt")
-		imgInfos = append(imgInfos, ImgInfo{urlOfImg, altOfImg})
-	})
-
 	return imgInfos
 }
 
+func getPicDatasInDocument(doc *goquery.Document) []ImgInfo {
+	var picInfos []ImgInfo
+	docPicture := doc.Find("picture")
+
+	docPicture.Each(func(i int, pic *goquery.Selection) {
+		urlOfImg, _ := pic.Attr("src")
+		altOfImg, _ := pic.Attr("alt")
+		picInfos = append(picInfos, ImgInfo{urlOfImg, altOfImg})
+	})
+
+	return picInfos
+}
+
 // rel = "nofollow" or rel="ugc" 속성 확인 필요
-func getURLsInDocument(doc *goquery.Document) []string {
+func getURLsInDocument(doc *goquery.Document, baseURL string) []string {
 	var URLs []string
 	docATags := doc.Find("a")
 
+	// URL 중복 제거 기능 필요하겠는데
 	docATags.Each(func(i int, tag *goquery.Selection) {
 		tempLink, _ := tag.Attr("href")
-		link = getCleansingURL(tempLink)
 
-		if link != "" :
+		if isValidURL(tempLink) {
+			link := GetCleansingURL(baseURL, tempLink)
+			fmt.Println(link)
 			URLs = append(URLs, link)
+		}
 	})
 
 	return URLs
+}
+
+func isValidURL(url string) bool {
+	//조건은 검색해보면서 더 추가해야겠네.
+	urlSplitWords := strings.Split(url, "/")
+	urlSplitWordCnt := len(urlSplitWords)
+
+	return urlSplitWordCnt > 1
 }
 
 /*
@@ -85,10 +103,56 @@ func getURLsInDocument(doc *goquery.Document) []string {
 *	Case 1 : URL 형식이 아닌 문자열 삭제 (예 : void(0))
 *	Case 2 : 기본 URL이 없는 문자열 정제 (예 : /img/example.png) => (base_url)http://demo.samplePage + (extract_url)/img/example.png
 *	Case 3 : 정상적인 URL 문자열 (예 : http://demo.samplePage/img/example.png)
-*/
-func getCleansingURL(url string) string{
-	return url
+ */
+func GetCleansingURL(baseURL string, url string) string {
+	cleansingURL := url
+
+	// 먼저 URL이 유효한지부터 확인을 해야겠구나.
+	if !strings.Contains(url, "http") {
+		cleansingURL = baseURL + url
+	}
+
+	return cleansingURL
 }
+
+func writeLinkInFile(links []string) {
+	file, err := os.Create("links.csv")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//에러 체크 함수 필요
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	for _, link := range links {
+		w.Write([]string{link})
+		//에러 체크 함수 필요
+	}
+
+}
+
+func writeImgInFile(imgInfos []ImgInfo) {
+	file, err := os.Create("imgInfos.csv")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	for _, imgInfo := range imgInfos {
+		w.Write([]string{imgInfo.url, imgInfo.alt})
+	}
+}
+
+/*
+*	함수 작성 필요
+*	논의 사항 - 데이터 입력 때 DB커넥션이 가장 적은 방안 논의 / insert, batch, loader
+ */
+func insertDataInDB() {}
 
 /*
 func getJsonInDocument(doc *goquery.Documnet) string{
